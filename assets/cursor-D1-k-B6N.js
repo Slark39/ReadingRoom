2f63a48849c2616974dc7516182482561665e3d1
slark39 <sanyscott4139@gmail.com>
2025-06-04 07:44:29 -0700

    'realvite'

--- assets/cursor-D1-k-B6N.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-DpI5l3Y1.js";import"./index-Df_QNia4.js";import"./index.es-Bb1TNczd.js";import"./index.es-C0OXOCFq.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
