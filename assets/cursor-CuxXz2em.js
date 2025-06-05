50b51b7e9716f9e89d633b87ea774ac371e11106
slark39 <sanyscott4139@gmail.com>
2025-06-04 17:54:26 -0700

    'chore:approveamount'

---
+++ assets/cursor-CuxXz2em.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-Cxtl_vIX.js";import"./index-FlEVacs5.js";import"./index.es-bj74DkIy.js";import"./index.es-DgqhBhjf.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
